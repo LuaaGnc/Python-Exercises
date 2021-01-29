142777ab73d4b2e8fd463e085b41eee52743910b
Luis-FutEng <luisgvs0302@gmail.com>
2021-01-29 18:11:57 +0000

    Final/Copy ex106.py

--- Exercicios/ex106.py
+++ Exercicios/ex106.py
@@ -1,20 +1,41 @@
+from time import sleep
 
-def ajuda(escolha):
-    help(escolha)
+cores = ('\033[m',              # 0 - Sem cores
+         '\033[0;30;41m',       # 1 - vermelho
+         '\033[0;30;42m',       # 2 - Verde
+         '\033[0;30;43m',       # 3 - Amarelo
+         '\033[0;30;44m',       # 4 - Azul
+         '\033[0;30;45m',       # 5 - Roxo
+         '\033[7;30m')          # 6 - Branco
 
 
-escolha = str(input('\u001b[42m Função ou Biblioteca:\u001b[43m'))
+def ajuda(com):
+    titulo(f'Acessando o manual do comando \'{com}\'', 4)
+    print(cores[6], end='')
+    help(com)
+    print(cores[0], end='')
+    sleep(2)
 
-print('')
-ajuda(escolha)
-print()
-while escolha != 'fim':
-    escolha = str(input('\u001b[42m Função ou Biblioteca:\u001b[43m'))
 
-    print('\u001b[41m')
-    ajuda(escolha)
-    print('\u001b')
+def titulo(msg, cor=0):
+    tamanho = len(msg) + 4
+    print(cores[cor], end='')
+    print('~' * tamanho)
+    print(f'  {msg}')
+    print('~' * tamanho)
+    print(cores[0], end='')
+    sleep(1)
 
-print(' ~~~~~~~~~ ')
-print(' ATÉ LOGO  ')
-print(' ~~~~~~~~~ ')
+
+comando = ''
+
+while True:
+    titulo('SISTEMA DE AJUDA PyHELP', 2)
+    comando = input('Função ou Biblioteca > ')
+
+    if comando.upper() == 'FIM':
+        break
+    else:
+        ajuda(comando)
+
+titulo('ATÉ LOGO!', 1)
