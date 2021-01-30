dbf9fe63529aa2ef7bfb9caf2ae54a6a416f4daf
Luis-FutEng <luisgvs0302@gmail.com>
2021-01-30 23:52:30 +0000

    FinalVersion -> Ex108.py

---
+++ Exercicios20/ex108/source.py
@@ -0,0 +1,7 @@
+import moeda_func
+
+preco = float(input('Digite o preço: R$ '))
+
+print(f'A metade de {moeda_func.moeda(preco)} é {moeda_func.moeda(moeda_func.metade(preco))}')
+print(f'O dobro de {moeda_func.moeda(preco)} é {moeda_func.moeda(moeda_func.dobro(preco))}')
+print(f'Aumentando 10%, temos {moeda_func.moeda(moeda_func.aumentar(preco, 10))}')
